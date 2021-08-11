8f0a311b5053518cbbb44d10c87544b5d6872f14
Marcus Pöhls <marcus.poehls@gmail.com>
2021-08-11 16:29:07 +0200

    export scaffoldApp function

--- bin/run.ts
+++ bin/run.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { app } from '../src/index'
+import { scaffoldApp } from '../src/index'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-app.run(['scaffold', ...process.argv.slice(2)])
+scaffoldApp()
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@
 import { Application } from '@supercharge/cedar'
 import { ScaffoldCommand } from './scaffold-app-command'
 
-export const app = new Application().useDefaultCommand(
-  new ScaffoldCommand()
-)
+/**
+ * Run the CLI command to scaffold a new Supercharge application.
+ */
+export async function scaffoldApp (input?: string[]): Promise<void> {
+  const args = [...(input ?? process.argv.slice(2))]
+
+  await new Application()
+    .add(new ScaffoldCommand())
+    .run(['scaffold', ...args])
+}
