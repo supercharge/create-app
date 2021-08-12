f8de58785ac537eb9cb1ab34d6752f627f0d8a13
Marcus Pöhls <marcus.poehls@gmail.com>
2021-08-12 12:14:27 +0200

    add test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,27 @@
 'use strict'
 
+const Path = require('path')
+const Sinon = require('sinon')
 const { test } = require('tap')
+const Fs = require('@supercharge/fs')
+const { scaffoldApp, app } = require('../dist/src')
 
-test('Create App', async () => {
-  test('todo', async t => {
-    t.skip()
-  })
+test('scaffoldApp exists', async t => {
+  t.ok(typeof scaffoldApp === 'function')
+})
+
+test('Create Supercharge App', async t => {
+  t.setTimeout(60000)
+
+  const appName = 'test-supercharge-app'
+  const appRoot = Path.resolve(process.cwd(), appName)
+  const terminateStub = Sinon.stub(app, 'terminate').returns()
+
+  await app.run(['scaffold', appName])
+
+  t.ok(await Fs.pathExists(appRoot))
+  t.ok(terminateStub.calledWith())
+
+  await Fs.removeDir(appRoot)
+  terminateStub.restore()
 })
