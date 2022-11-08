5e875e4af8ca854966b481f4d42602b8d8c45d56
Marcus Pöhls <mp@seda.digital>
2022-11-08 14:44:21 +0100

    calculate milliseconds

--- test/helpers/timeout.js
+++ test/helpers/timeout.js
@@ -1,13 +1,21 @@
 'use strict'
 
-module.exports = (seconds, handler) => {
+const ms = require('ms')
+
+exports.timeout = function (duration, handler) {
+  const milliseconds = typeof duration === 'string'
+    ? ms(duration)
+    : duration
+
   return (context) => {
     let timer
 
     return Promise.race([
       handler(context),
-      new Promise((resolve, reject) => {
-        timer = setTimeout(() => reject(new Error('timeout')), seconds * 1000)
+      new Promise((_resolve, reject) => {
+        timer = setTimeout(() => {
+          reject(new Error(`Test timeout of "${milliseconds}" ms exceeded`))
+        }, milliseconds)
       })
     ]).finally(() => {
       clearTimeout(timer)
